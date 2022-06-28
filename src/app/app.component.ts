b2d548a43ae046376c54cc9e3661d5ae1ee40351
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-06-28 14:39:35 -0500

    viewchildren with changes to it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, QueryList, ViewChild, ViewChildren } from "@angular/core";
 import { COURSES } from "../db-data";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { Course } from "./model/course";
@@ -8,13 +8,31 @@ import { Course } from "./model/course";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
   @ViewChild("cardRef")
   card: CourseCardComponent;
 
-  onCourseSelected(course: Course) {
-    console.log(this.card);
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
+
+  ngAfterViewInit() {
+    this.cards.changes.subscribe((cards) => console.log(cards));
+  }
+
+  onCourseSelected(course: Course) {}
+
+  onCourseEdited() {
+    this.courses.push({
+      id: 1,
+      description: "Angular Core Deep Dive",
+      iconUrl:
+        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png",
+      longDescription:
+        "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: "INTERMEDIATE",
+      lessonsCount: 10,
+    });
   }
 }
