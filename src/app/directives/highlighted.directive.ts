6393afd9be27b5ee24094b60a51947d2a19e91c6
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-06-30 20:52:35 -0500

    directives

---
+++ src/app/directives/highlighted.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostBinding, Input } from "@angular/core";
+
+@Directive({
+  selector: "[highlighted]",
+})
+export class HighlightedDirective {
+  @Input("highlighted")
+  isHighlighted = false;
+
+  constructor() {
+    console.log("directive created");
+  }
+
+  @HostBinding("class.highlighted")
+  get cssClasses() {
+    return this.isHighlighted;
+  }
+}
